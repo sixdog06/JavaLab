5af3dc9b4d0bd2bea26a086921b98525905175d2
huanruiz <huanruiz@foxmail.com>
2021-11-09 20:40:59 +0800

    Item 4

--- effective-java/src/main/java/com/punchcode/effectivejava/chapter2/Item4.java
+++ effective-java/src/main/java/com/punchcode/effectivejava/chapter2/Item4.java
@@ -1,8 +1,22 @@
 package com.punchcode.effectivejava.chapter2;
 
+import com.punchcode.effectivejava.chapter2.common.UtilityClass;
+
+import java.util.Arrays;
+
 /**
  * @author huanruiz
  * @since 2021/11/9
  */
 public class Item4 {
+
+    public static void main(String[] args) {
+        //UtilityClass isntance = new UtilityClass();
+        UtilityClass.staticMethod();
+
+        //Math, Arrays
+
+        // 类的内部也无法实例化
+        // UtilityClass.wrongMethod();
+    }
 }
---
+++ effective-java/src/main/java/com/punchcode/effectivejava/chapter2/common/UtilityClass.java
@@ -0,0 +1,21 @@
+package com.punchcode.effectivejava.chapter2.common;
+
+/**
+ * @author huanruiz
+ * @since 2021/11/9
+ */
+public class UtilityClass {
+
+    // 不能实例化
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+
+    public static void staticMethod() {
+        System.out.println("this is a static method");
+    }
+
+    public static void wrongMethod() {
+        new UtilityClass();
+    }
+}
