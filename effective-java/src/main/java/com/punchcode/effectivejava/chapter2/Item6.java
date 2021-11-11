df311df9f29a5e97e15fdf8659283c77d38e29a6
huanruiz <huanruiz@foxmail.com>
2021-11-11 13:34:54 +0800

    item 6

--- effective-java/src/main/java/com/punchcode/effectivejava/chapter2/Item6.java
+++ effective-java/src/main/java/com/punchcode/effectivejava/chapter2/Item6.java
@@ -1,6 +1,9 @@
 package com.punchcode.effectivejava.chapter2;
 
+import com.punchcode.effectivejava.chapter2.common.RomanNumerals;
+
 /**
+ * Item 6: Avoid creating unnecessary objects
  * @author huanruiz
  * @since 2021/11/10
  */
@@ -8,5 +11,23 @@ public class Item6 {
 
     public static void main(String[] args) {
 
+        // 多创建了一个实例
+        String str1 = new String("bikini");
+
+        // 在字符串常量池可复用, 同样道理, 也要考虑静态工厂方法
+        String str2 = "bikini";
+
+        // 开销大的, 要重复是用的对象只创建一次
+        RomanNumerals.isRomanNumeral(str2);
+    }
+
+    private static long sum() {
+        // 应该用long
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++) {
+            // 频繁装箱, 影响性能
+            sum += i;
+        }
+        return sum;
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effectivejava/chapter2/common/RomanNumerals.java
@@ -0,0 +1,19 @@
+package com.punchcode.effectivejava.chapter2.common;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author huanruiz
+ * @since 2021/11/11
+ */
+public class RomanNumerals {
+
+    // 只创建一次, 多次创建会多次使用有限状态机, 开销大
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    public static boolean isRomanNumeral(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+}
