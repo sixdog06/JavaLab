ee9d97a16419dbfaf4d8c846b30abab5ca2714bd
huanruiz <huanruiz@foxmail.com>
2021-12-13 13:04:32 +0800

    item 26

---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter5/Item26.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter5;
+
+/**
+ * Item 26: Don’t use raw types
+ * @author huanruiz
+ * @since 2021/12/13
+ */
+public class Item26 {
+
+    public static void main(String[] args) {
+
+    }
+}
