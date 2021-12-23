926d0a01a7860cfdca5a618ad585944c14d37756
huanruiz <huanruiz@foxmail.com>
2021-12-23 10:28:35 +0800

    item32

--- effective-java/src/main/java/com/punchcode/effective_java/chapter5/Item31.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter5/Item31.java
@@ -2,10 +2,7 @@ package com.punchcode.effective_java.chapter5;
 
 import com.punchcode.effective_java.chapter5.common.Stack;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Item 31: Use bounded wildcards to increase API flexibility
@@ -30,5 +27,34 @@ public class Item31 {
         List<Number> dest = new ArrayList<>();
         stack.popAll(dest);
         System.out.println(dest);
+
+        /**
+         * wildcard or not?
+         */
+        List<Integer> list1 = Arrays.asList(1, 2);
+        List<Integer> list2 = Arrays.asList(1, 2);
+        swap2(list1, 1, 0);
+        swap2(list2, 1, 0);
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+
+    public static <E> void swap1(List<E> list, int i, int j) {
+        list.set(i, list.set(j, list.get(i)));
+    }
+
+    /**
+     * 对公共api来说, 这种实现方式比{@code swap1}更加灵活. todo: swap哪里不灵活了, 书里面没给例子, 我也想不出...
+     */
+    public static void swap2(List<?> list, int i, int j) {
+        swapHelper(list, i, j);
+    }
+
+    /**
+     * {@code List<?>}的限制, 所以无法把非null的值set进list, 那么需要{@code swapHelper}去设置,
+     * {@code swapHelper}知道list是{@code List<E>}这个类型.
+     */
+    private static <E> void swapHelper(List<E> list, int i, int j) {
+        list.set(i, list.set(j, list.get(i)));
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter5/Item32.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter5;
+
+/**
+ * Item 32: Combine generics and varargs judiciously
+ * @author huanruiz
+ * @since 2021/12/23
+ */
+public class Item32 {
+
+    public static void main(String[] args) {
+
+    }
+}
