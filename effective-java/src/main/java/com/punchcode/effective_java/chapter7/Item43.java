6df681e01e7a6e07efe01fbcfe36b460a23aabe6
huanruiz <huanruiz@foxmail.com>
2022-01-12 13:04:19 +0800

    start item 44

--- effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item43.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item43.java
@@ -1,5 +1,8 @@
 package com.punchcode.effective_java.chapter7;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Item 43: Prefer method references to lambdas
  * @author huanruiz
@@ -7,7 +10,15 @@ package com.punchcode.effective_java.chapter7;
  */
 public class Item43 {
 
-    public static void main(String[] args) {
+    private static final String KEY = "key";
 
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>(5);
+        map.put(KEY, 2);
+        // 长点
+        map.merge(KEY, 1, (count, incr) -> count + incr);
+        // 短点
+        map.merge(KEY, 1, Integer::sum);
+        System.out.println(map);
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item44.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter7;
+
+/**
+ * Item 44: Favor the use of standard functional interfaces
+ * @author huanruiz
+ * @since 2022/1/12
+ */
+public class Item44 {
+
+    public static void main(String[] args) {
+
+    }
+}
