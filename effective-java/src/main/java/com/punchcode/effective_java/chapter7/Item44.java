c3114e92544f15ae5a39630251c293d3ea659160
huanruiz <huanruiz@foxmail.com>
2022-01-13 20:21:47 +0800

    start item45

--- effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item44.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item44.java
@@ -1,13 +1,32 @@
 package com.punchcode.effective_java.chapter7;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
 /**
- * Item 44: Favor the use of standard functional interfaces
+ * Item 44: Favor the use of standard functional
  * @author huanruiz
  * @since 2022/1/12
  */
-public class Item44 {
+public class Item44 implements Predicate<String> {
+
+    private static final String STRING_I_WANT = "I am happy";
+
+    private static final String STRING_I_DONT_WANT = "I am sad";
 
     public static void main(String[] args) {
+        List<String> list = Arrays.asList(STRING_I_WANT, STRING_I_DONT_WANT);
+        list.stream().filter(new Item44()).forEach(System.out::println);
+    }
 
+    @Override
+    public boolean test(String str) {
+        if (Objects.equals(str, STRING_I_WANT)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter7/Item45.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter7;
+
+/**
+ * Item 45: Use streams judiciously
+ * @author huanruiz
+ * @since 2022/1/13
+ */
+public class Item45 {
+
+    public static void main(String[] args) {
+
+    }
+}
