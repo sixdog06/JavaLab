12e2c8e05f6b30180fad53f35e818eff5560a91b
huanruiz <huanruiz@foxmail.com>
2022-01-18 10:14:17 +0800

    start item52

--- effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item52.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item52.java
@@ -1,13 +1,13 @@
 package com.punchcode.effective_java.chapter8;
 
 /**
- * Item 51: Design method signatures carefully
+ * Item 52: Use overloading judiciously
  * @author huanruiz
- * @since 2022/1/17
+ * @since 2022/1/18
  */
-public class Item51 {
+public class Item52 {
 
     public static void main(String[] args) {
-        // nothing to write
+
     }
 }
