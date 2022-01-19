6b8f190efab938064ef9dc18d1aa1e5c5bd1b741
huanruiz <huanruiz@foxmail.com>
2022-01-19 09:40:37 +0800

    start item54

--- effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item53.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item53.java
@@ -8,6 +8,43 @@ package com.punchcode.effective_java.chapter8;
 public class Item53 {
 
     public static void main(String[] args) {
+        System.out.println(sum());
+        System.out.println(min(1));
+        System.out.println(min2(1));
+    }
 
+    static int sum(int... args) {
+        int sum = 0;
+        for (int arg : args) {
+            sum += arg;
+        }
+        return sum;
+    }
+
+    /**
+     * The WRONG way to use varargs to pass one or more arguments!
+     */
+    static int min(int... args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Too few arguments");
+        }
+        int min = args[0];
+        for (int i = 1; i < args.length; i++) {
+            if (args[i] < min) {
+                min = args[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * The right way to use varargs to pass one or more arguments
+     */
+    static int min2(int firstArg, int... remainingArgs) {
+        int min = firstArg;
+        for (int arg : remainingArgs)
+            if (arg < min)
+                min = arg;
+        return min;
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item54.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter8;
+
+/**
+ * Item 54: Return empty collections or arrays, not nulls
+ * @author huanruiz
+ * @since 2022/1/19
+ */
+public class Item54 {
+
+    public static void main(String[] args) {
+
+    }
+}
