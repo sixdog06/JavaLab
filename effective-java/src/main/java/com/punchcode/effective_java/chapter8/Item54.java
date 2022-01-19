520d255444724ee6cf05286366b8a9677074bc73
huanruiz <huanruiz@foxmail.com>
2022-01-19 10:06:27 +0800

    start item55

--- effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item54.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item54.java
@@ -1,5 +1,9 @@
 package com.punchcode.effective_java.chapter8;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Item 54: Return empty collections or arrays, not nulls
  * @author huanruiz
@@ -9,5 +13,20 @@ public class Item54 {
 
     public static void main(String[] args) {
 
+    }
+
+    /**
+     * 为null返回空list
+     */
+    public List<String> getCheeses(List<String> cheesesInStock) {
+        return cheesesInStock.isEmpty() ? Collections.emptyList()
+                : new ArrayList<>(cheesesInStock);
+    }
+
+    /**
+     * 为null返回空array, 如果还觉得影响性能, 那么zero-length arrays定义为静态常量反复使用
+     */
+    public String[] getCheesesArray(List<String>  cheesesInStock) {
+        return cheesesInStock.toArray(new String[0]);
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter8/Item55.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter8;
+
+/**
+ * Item 55: Return optionals judiciously
+ * @author huanruiz
+ * @since 2022/1/19
+ */
+public class Item55 {
+
+    public static void main(String[] args) {
+
+    }
+}
