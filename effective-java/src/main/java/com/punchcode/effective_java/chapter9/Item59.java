73b5337ce12b37d4cd42b2205e8e3fbf03f2a14f
huanruiz <huanruiz@foxmail.com>
2022-01-23 15:06:45 +0800

    Item60

--- effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item59.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item59.java
@@ -1,5 +1,9 @@
 package com.punchcode.effective_java.chapter9;
 
+import java.util.Random;
+import java.util.SplittableRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Item 59: Know and use the libraries
  * @author huanruiz
@@ -7,7 +11,44 @@ package com.punchcode.effective_java.chapter9;
  */
 public class Item59 {
 
-    public static void main(String[] args) {
+    /**
+     * Common but deeply flawed!
+     */
+    static Random rnd = new Random();
 
+    static SplittableRandom rnd2 = new SplittableRandom();
+
+    public static void main(String[] args) {
+        int n = 2 * (Integer.MAX_VALUE / 3);
+        int low = 0;
+        int low2 = 0;
+        int low3 = 0;
+        int low4 = 0;
+        for (int i = 0; i < 1000000; i++) {
+            if (random(n) < n / 2) {
+                low++;
+            }
+            if (rnd.nextInt(n) < n / 2) {
+                low2++;
+            }
+            if (ThreadLocalRandom.current().nextInt(n) < n / 2) {
+                low3++;
+            }
+            if (rnd2.nextInt(n) < n / 2) {
+                low4++;
+            }
+        }
+        // 输出应该是500000左右, 但实际上是666666左右
+        System.out.println(low);
+        // 正确调用
+        System.out.println(low2);
+        // 性能更好的调用
+        System.out.println(low3);
+        // fork join pools and parallel streams调用
+        System.out.println(low4);
+    }
+
+    static int random(int n) {
+        return Math.abs(rnd.nextInt()) % n;
     }
 }
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item60.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter9;
+
+/**
+ * Item 60: Avoid float and double if exact answers
+ * @author huanruiz
+ * @since 2022/1/23
+ */
+public class Item60 {
+
+    public static void main(String[] args) {
+
+    }
+}
