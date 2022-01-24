2ac4f501177e8ed3faf0d02af9c0854124a11d98
huanruiz <huanruiz@foxmail.com>
2022-01-24 10:08:21 +0800

    start item65

--- effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item63.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item63.java
@@ -1,11 +1,11 @@
 package com.punchcode.effective_java.chapter9;
 
 /**
- * Item 62: Avoid strings where other types are more appropriate
+ * Item 63: Beware the performance of string concatenation
  * @author huanruiz
- * @since 2022/1/23
+ * @since 2022/1/24
  */
-public class Item62 {
+public class Item63 {
 
     public static void main(String[] args) {
         // nothing to write
---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item64.java
@@ -0,0 +1,17 @@
+package com.punchcode.effective_java.chapter9;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Item 64: Refer to objects by their interfaces
+ * @author huanruiz
+ * @since 2022/1/24
+ */
+public class Item64 {
+
+    public static void main(String[] args) {
+        // nothing to write
+    }
+}
