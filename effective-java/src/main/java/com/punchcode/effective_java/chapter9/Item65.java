197ac48bd3eb8e7beb9ed44019eced31c8792fe7
huanruiz <huanruiz@foxmail.com>
2022-01-24 13:17:27 +0800

    start item67

--- effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item65.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item65.java
@@ -1,5 +1,10 @@
 package com.punchcode.effective_java.chapter9;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+
 /**
  * Item 65: Prefer interfaces to reflection
  * @author huanruiz
@@ -7,7 +12,47 @@ package com.punchcode.effective_java.chapter9;
  */
 public class Item65 {
 
+    /**
+     * Reflective instantiation with interface access
+     */
     public static void main(String[] args) {
+        // Translate the class name into a Class object
+        Class<? extends Set<String>> cl = null;
+        try {
+            cl = (Class<? extends Set<String>>)Class.forName(args[0]); // Unchecked cast!
+        } catch (ClassNotFoundException e) {
+            fatalError("Class not found.");
+        }
 
+        // Get the constructor
+        Constructor<? extends Set<String>> cons = null;
+        try {
+            cons = cl.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            fatalError("No parameterless constructor");
+        }
+
+        // Instantiate the set
+        Set<String> s = null;
+        try {
+            s = cons.newInstance();
+        } catch (IllegalAccessException e) {
+            fatalError("Constructor not accessible");
+        } catch (InstantiationException e) {
+            fatalError("Class not instantiable.");
+        } catch (InvocationTargetException e) {
+            fatalError("Constructor threw " + e.getCause());
+        } catch (ClassCastException e) {
+            fatalError("Class doesn't implement Set");
+        }
+
+        // Exercise the set, 使用时就不用在意反射的逻辑了
+        s.addAll(Arrays.asList(args).subList(1, args.length));
+        System.out.println(s);
+    }
+
+    private static void fatalError(String msg) {
+        System.err.println(msg);
+        System.exit(1);
     }
 }
