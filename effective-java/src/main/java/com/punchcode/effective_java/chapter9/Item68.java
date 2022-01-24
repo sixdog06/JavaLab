3a69a5db9935c132f1c09c13172dd84baab32c4a
huanruiz <huanruiz@foxmail.com>
2022-01-24 13:26:28 +0800

    start item68

--- effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item68.java
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter9/Item68.java
@@ -1,13 +1,13 @@
 package com.punchcode.effective_java.chapter9;
 
 /**
- * Item 67: Optimize judiciously
+ * Item 68: Adhere to generally accepted naming conventions
  * @author huanruiz
  * @since 2022/1/24
  */
-public class Item67 {
+public class Item68 {
 
     public static void main(String[] args) {
-        // nothing to write
+
     }
 }
