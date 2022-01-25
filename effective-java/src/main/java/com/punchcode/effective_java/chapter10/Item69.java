461ac36b3769dd73bfa091429fe3d486fdb19662
huanruiz <huanruiz@foxmail.com>
2022-01-25 10:06:12 +0800

    finish item68

---
+++ effective-java/src/main/java/com/punchcode/effective_java/chapter10/Item69.java
@@ -0,0 +1,13 @@
+package com.punchcode.effective_java.chapter10;
+
+/**
+ * Item 69: Use exceptions only for exceptional conditions
+ * @author huanruiz
+ * @since 2022/1/25
+ */
+public class Item69 {
+
+    public static void main(String[] args) {
+
+    }
+}
