aee958eb7e6e684c460dc4c70c7bfeb05e299c06
huanruiz <huanruiz@foxmail.com>
2022-01-29 21:46:34 +0800

    concurrency

---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter2/AtomicTest.java
@@ -0,0 +1,19 @@
+package com.punchcode.java_concurrency_in_practice.chapter2;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author huanruiz
+ * @since 2022/1/29
+ */
+public class AtomicTest {
+
+    /**
+     * 使用原子变量类, 保证原子性
+     */
+    private final AtomicLong count = new AtomicLong(0);
+
+    public long getCount() {
+        return count.getAndIncrement();
+    }
+}
