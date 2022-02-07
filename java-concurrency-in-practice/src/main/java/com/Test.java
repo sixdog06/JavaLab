e545101ba9f918f95cbb8233a85d8aec4cbdfddc
huanruiz <huanruiz@foxmail.com>
2022-02-07 21:19:27 +0800

    带缓存且线程安全的因式分解Servlet

---
+++ java-concurrency-in-practice/src/main/java/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author huanruiz
+ * @since 2022/2/7
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
--- java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter2/CachedFactorizer.java
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter2/CachedFactorizer.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.Arrays;
 
 /**
- * 线程安全的因式分解Servlet
+ * 带缓存且线程安全的因式分解Servlet
+ * 既没有使用原子变量类, 也没有对整个方法加锁, 把栈上变量(每个线程独有的变量)排除在锁之外
+ * 符合我们非共享共享不加锁的原则
  * @author huanruiz
  * @since 2022/2/7
  */
@@ -27,6 +30,7 @@ public class CachedFactorizer extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BigInteger i = (BigInteger)req.getAttribute("i");
         BigInteger[] factors = null;
+        // 查询, 先检查后执行
         synchronized (this) {
             ++hits;
             if (i.equals(lastNumber)) {
@@ -34,20 +38,18 @@ public class CachedFactorizer extends HttpServlet {
                 factors = lastFactors.clone();
             }
         }
+
         if (factors == null) {
             factors = factor(i);
+            // 修改, 实时更新缓存
             synchronized (this) {
                 lastNumber = i;
                 lastFactors = factors.clone();
             }
         }
         PrintWriter writer = resp.getWriter();
-        writer.print(factors);
-        super.doPost(req, resp);
-    }
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
+        writer.print(Arrays.toString(factors));
+        super.doGet(req, resp);
     }
 
     public synchronized long getHits() {
