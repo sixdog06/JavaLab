afb895ff5945b6fd515e4d3df27d5c4d6be5c37e
huanruiz <huanruiz@foxmail.com>
2022-02-09 22:11:42 +0800

    local thread

---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter3/ThreadConfinementExample.java
@@ -0,0 +1,55 @@
+package com.punchcode.java_concurrency_in_practice.chapter3;
+
+import com.punchcode.java_concurrency_in_practice.chapter3.common.Animal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * @author huanruiz
+ * @since 2022/2/9
+ */
+public class ThreadConfinementExample {
+
+    private static final String DB_URL = "haha.com";
+
+    /**
+     * animals被封闭在栈内了
+     */
+    public int loadTheArk(Collection<Animal> candidates) {
+        SortedSet<Animal> animals;
+        int numPairs = 0;
+        Animal candidate = null;
+
+        // animals confined to method, don’t let them escape!
+        animals = new TreeSet<>();
+        animals.addAll(candidates);
+        for (Animal a : animals) {
+            if (candidate == null || !candidate.isPotentialMate(a)) {
+                candidate = a;
+            }
+        }
+        return numPairs;
+    }
+
+    private static ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>() {
+
+        @Override
+        public Connection initialValue() {
+            try {
+                return DriverManager.getConnection(DB_URL);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
+
+    public static Connection getConnection() {
+        return connectionHolder.get();
+    }
+}
---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter3/common/Animal.java
@@ -0,0 +1,12 @@
+package com.punchcode.java_concurrency_in_practice.chapter3.common;
+
+/**
+ * @author huanruiz
+ * @since 2022/2/9
+ */
+public class Animal {
+
+    public boolean isPotentialMate(Animal a) {
+        return true;
+    }
+}
