a5bdddcad4a5466cf1fdbe753c7d6cf9a669b23d
huanruiz <huanruiz@foxmail.com>
2022-02-14 10:05:18 +0800

    Counter

--- java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter4/Counter.java
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter4/Counter.java
@@ -2,11 +2,17 @@ package com.punchcode.java_concurrency_in_practice.chapter4;
 
 /**
  * 监视器模式的线程安全计数器
+ * synchronization policy defines how an object coordinates access to its state without
+ * violating its invariants or postconditions. It specifies the combination of immutability,
+ * thread confinement, and locking.
  * @author huanruiz
  * @since 2022/2/11
  */
 public final class Counter {
 
+    /**
+     * long是primitive type, 所以value是构成这个对象的所有状态
+     */
     private long value = 0;
 
     public synchronized long getValue() {
