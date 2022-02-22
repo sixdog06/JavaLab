b172a27e65f2e072c5ad1e867b2ebaf2c7fc2925
huanruiz <huanruiz@foxmail.com>
2022-02-22 09:49:05 +0800

    TaskRunnable

---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter5/TaskRunnable.java
@@ -0,0 +1,27 @@
+package com.punchcode.java_concurrency_in_practice.chapter5;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * 恢复中断
+ * @author huanruiz
+ * @since 2022/2/22
+ */
+public class TaskRunnable implements Runnable {
+
+    BlockingQueue<String> queue;
+
+    @Override
+    public void run() {
+        try {
+            processTask(queue.take());
+        } catch (InterruptedException e) {
+            // restore interrupted status
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void processTask(String take) {
+        // do something
+    }
+}
