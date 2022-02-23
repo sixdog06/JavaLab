8c3b97f2ab625c0a4c06c550f24bc2c4f08d64e8
huanruiz <huanruiz@foxmail.com>
2022-02-23 10:11:47 +0800

    TestHarness

---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter5/TestHarness.java
@@ -0,0 +1,37 @@
+package com.punchcode.java_concurrency_in_practice.chapter5;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author huanruiz
+ * @since 2022/2/23
+ */
+public class TestHarness {
+
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        startGate.await();
+                        try {
+                            task.run();
+                        } finally {
+                            endGate.countDown();
+                        }
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            };
+            t.start();
+        }
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
