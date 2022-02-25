b7d9bddde9884326be4924d93d04fb28678cf38f
huanruiz <huanruiz@foxmail.com>
2022-02-25 10:04:48 +0800

    Semaphore

---
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter5/BoundedHashSet.java
@@ -0,0 +1,48 @@
+package com.punchcode.java_concurrency_in_practice.chapter5;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author huanruiz
+ * @since 2022/2/25
+ */
+public class BoundedHashSet<T> {
+
+    private final Set<T> set;
+
+    private final Semaphore sem;
+
+    /**
+     * @param bound 信号量的限制值
+     */
+    public BoundedHashSet(int bound) {
+        this.set = Collections.synchronizedSet(new HashSet<T>());
+        sem = new Semaphore(bound);
+    }
+
+    public boolean add(T o) throws InterruptedException {
+        sem.acquire();
+        boolean wasAdded = false;
+        try {
+            wasAdded = set.add(o);
+            return wasAdded;
+        }
+        finally {
+            if (!wasAdded) {
+                sem.release();
+            }
+        }
+    }
+
+    public boolean remove(Object o) {
+        boolean wasRemoved = set.remove(o);
+        if (wasRemoved) {
+            sem.release();
+        }
+        return wasRemoved;
+    }
+}
+
