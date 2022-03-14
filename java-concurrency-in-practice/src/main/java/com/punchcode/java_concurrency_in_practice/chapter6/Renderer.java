b1b0bbbf9a022f984206578911134b73a24fdb5f
huanruiz <huanruiz@foxmail.com>
2022-03-14 19:55:16 +0800

    renderer

--- java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter6/Renderer.java
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter6/Renderer.java
@@ -1,4 +1,7 @@
-package com.punchcode.java_concurrency_in_practice.chapter6.common;
+package com.punchcode.java_concurrency_in_practice.chapter6;
+
+import com.punchcode.java_concurrency_in_practice.chapter6.common.ImageData;
+import com.punchcode.java_concurrency_in_practice.chapter6.common.ImageInfo;
 
 import java.util.List;
 import java.util.concurrent.*;
@@ -25,26 +28,28 @@ public class Renderer {
         CompletionService<ImageData> completionService = new ExecutorCompletionService<>(executor);
         
         for (final ImageInfo imageInfo : info) {
+            // 下载任务拆线程做
             completionService.submit(new Callable<ImageData>() {
                 @Override
                 public ImageData call() {
                     return imageInfo.downloadImage();
                 }
             });
+        }
     
-            renderText(source);
+        renderText(source);
     
-            try {
-                for (int t = 0, n = info.size(); t < n; t++) {
-                    Future<ImageData> f = completionService.take();
-                    ImageData imageData = f.get();
-                    renderImage(imageData);
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } catch (ExecutionException e) {
-                throw launderThrowable(e.getCause());
+        try {
+            for (int t = 0, n = info.size(); t < n; t++) {
+                // 把已经下载成功的图片分别渲染, 消费阻塞队列
+                Future<ImageData> f = completionService.take();
+                ImageData imageData = f.get();
+                renderImage(imageData);
             }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            throw launderThrowable(e.getCause());
         }
     }
 }
