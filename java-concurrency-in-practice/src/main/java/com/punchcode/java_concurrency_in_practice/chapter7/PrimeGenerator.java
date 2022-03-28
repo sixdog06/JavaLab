8e3a0ebfc9ee9c5dc0f231b12a3668e69c25c471
huanruiz <huanruiz@foxmail.com>
2022-03-28 21:22:17 +0800

    PrimeGenerator

--- java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter7/PrimeGenerator.java
+++ java-concurrency-in-practice/src/main/java/com/punchcode/java_concurrency_in_practice/chapter7/PrimeGenerator.java
@@ -4,16 +4,20 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 /**
+ * 质数生成器
  * @author huanruiz
  * @since 2022/3/23
  */
-public class PrimeGenerator {
+public class PrimeGenerator implements Runnable {
     
     private final List<BigInteger> primes = new ArrayList<>();
     
     private volatile boolean cancelled;
     
+    @Override
     public void run() {
         BigInteger p = BigInteger.ONE;
         while (!cancelled) {
@@ -29,6 +33,20 @@ public class PrimeGenerator {
     }
     
     public synchronized List<BigInteger> get() {
-        return new ArrayList<BigInteger>(primes);
+        return new ArrayList<>(primes);
+    }
+    
+    /**
+     * 1秒钟的时候调用cancel
+     */
+    List<BigInteger> aSecondOfPrimes() throws InterruptedException {
+        PrimeGenerator generator = new PrimeGenerator();
+        new Thread(generator).start();
+        try {
+            SECONDS.sleep(1);
+        } finally {
+            generator.cancel();
+        }
+        return generator.get();
     }
 }
