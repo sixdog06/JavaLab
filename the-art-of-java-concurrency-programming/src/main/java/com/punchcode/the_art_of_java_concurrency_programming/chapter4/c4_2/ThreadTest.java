9e5f203b42fd9c313015037e736ac954ba0e778f
huanruiz <huanruiz@foxmail.com>
2023-03-21 13:38:38 +0800

    c4_2

---
+++ the-art-of-java-concurrency-programming/src/main/java/com/punchcode/the_art_of_java_concurrency_programming/chapter4/c4_2/ThreadTest.java
@@ -0,0 +1,10 @@
+package com.punchcode.the_art_of_java_concurrency_programming.chapter4.c4_2;
+
+/**
+ * @author: Harry Zhang
+ * @since: 21/Mar/2023
+ */
+public class ThreadTest {
+
+    Thread thread = new Thread();
+}
