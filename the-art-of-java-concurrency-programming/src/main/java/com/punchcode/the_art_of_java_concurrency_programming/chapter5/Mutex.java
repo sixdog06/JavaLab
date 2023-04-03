0898363dac97a1e7905afc035c40debc4d55277c
huanruiz <huanruiz@foxmail.com>
2023-04-03 20:05:54 +0800

    update

--- the-art-of-java-concurrency-programming/src/main/java/com/punchcode/the_art_of_java_concurrency_programming/chapter5/Mutex.java
+++ the-art-of-java-concurrency-programming/src/main/java/com/punchcode/the_art_of_java_concurrency_programming/chapter5/Mutex.java
@@ -12,7 +12,9 @@ import java.util.concurrent.locks.Lock;
  */
 public class Mutex implements Lock {
 
-    // 静态内部类, 自定义同步器
+    /**
+     * 静态内部类, 自定义同步器
+     */
     private static class Sync extends AbstractQueuedSynchronizer {
 
         // 是否处于占用状态
@@ -51,15 +53,33 @@ public class Mutex implements Lock {
     // 仅需要将操作代理到Sync上即可
     private final Sync sync = new Sync();
     @Override
-    public void lock() { sync.acquire(1); }
+    public void lock() {
+        sync.acquire(1);
+    }
+
     @Override
-    public boolean tryLock() { return sync.tryAcquire(1); }
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
     @Override
-    public void unlock() { sync.release(1); }
+    public void unlock() {
+        sync.release(1);
+    }
+
     @Override
-    public Condition newCondition() { return sync.newCondition(); }
-    public boolean isLocked() { return sync.isHeldExclusively(); }
-    public boolean hasQueuedThreads() { return sync.hasQueuedThreads(); }
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
     @Override
     public void lockInterruptibly() throws InterruptedException {
         sync.acquireInterruptibly(1);
